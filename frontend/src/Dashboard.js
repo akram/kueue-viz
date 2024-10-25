07566d83b6c9119fa2ea03de01e32aad84c73b8a
Akram Ben Aissi <akram.benaissi@gmail.com>
2024-10-26 00:09:29 +0100

    add notification

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Grid, Paper, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Dashboard = () => {
   const [queues, setQueues] = useState([]);
@@ -13,10 +14,16 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        console.error("AAAAAAAAAAA:", `${process.env.REACT_APP_BACKEND_URL}`);
+        console.error("REACT_APP_BACKEND_URL:", `${process.env.REACT_APP_BACKEND_URL}`);
         const response = await axios.get('http://backend-kueue-viz.apps.rosa.akram.s25d.p3.openshiftapps.com/kueue/status');
         setQueues(response.data.queues.items || []);
         setWorkloads(response.data.workloads.items || []);
+        // Check for preempted workloads and trigger a notification
+        response.data.workloads.items.forEach(workload => {
+          if (workload.preemption?.preempted) {
+            toast.error(`Workload ${workload.metadata.name} was preempted: ${workload.preemption.reason}`);
+          }
+        });
       } catch (error) {
         setError('Failed to fetch data');
         console.error("Error fetching data:", error);
@@ -32,6 +39,7 @@ const Dashboard = () => {
 
   return (
     <>
+      <ToastContainer />
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6} md={4}>
           <Paper elevation={3} style={{ padding: '16px' }}>
