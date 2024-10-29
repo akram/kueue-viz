1a62774330f8727b8bc8b2824a9257977ab62d99
Akram Ben Aissi <akram.benaissi@gmail.com>
2024-10-29 18:51:43 +0100

    fix

--- frontend/src/WorkloadsList.js
+++ frontend/src/WorkloadsList.js
@@ -1,5 +1,5 @@
+import { Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from '@mui/material';
 import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper ,Tooltip} from '@mui/material';
 
 const WorkloadsList = ({ workloads }) => {
   return (
@@ -21,22 +21,22 @@ const WorkloadsList = ({ workloads }) => {
             <TableRow key={workload.metadata.name}>
               <TableCell>
                 <Tooltip
-                title={
+                  title={
                     <div>
-                    <div><strong>Pod Sets Count:</strong> {workload.spec?.podSets?.[0]?.count || 'N/A'}</div>
-                    <div><strong>Owner Reference: {workload.ownerReferences?.[0]?.uid || 'N/A'}</strong></div>
-                    <div>API Version: {workload.ownerReferences?.[0]?.apiVersion || 'N/A'}</div>
-                    <div>Kind: {workload.ownerReferences?.[0]?.kind || 'N/A'}</div>
-                    <div>Name: {workload.ownerReferences?.[0]?.name || 'N/A'}</div>
+                      <div><strong>Pod Sets Count:</strong> {workload.spec?.podSets?.[0]?.count || 'N/A'}</div>
+                      <div><strong>Owner Reference: {workload.ownerReferences?.[0]?.uid || 'N/A'}</strong></div>
+                      <div>API Version: {workload.ownerReferences?.[0]?.apiVersion || 'N/A'}</div>
+                      <div>Kind: {workload.ownerReferences?.[0]?.kind || 'N/A'}</div>
+                      <div>Name: {workload.ownerReferences?.[0]?.name || 'N/A'}</div>
                     </div>
-                }
-                arrow
+                  }
+                  arrow
                 >
-                <Link to={`/workload/${workload.metadata.name}`}>
+                  <Link to={`/workload/${workload.metadata.name}`}>
                     <span style={{ cursor: 'pointer', textDecoration: 'underline', color: 'blue' }}>
-                    {workload.metadata.name}
+                      {workload.metadata.name}
                     </span>
-                </Link>
+                  </Link>
                 </Tooltip>
               </TableCell>
               <TableCell><Link to={`/local-queue/${workload.spec.queueName}`}>{workload.spec.queueName}</Link></TableCell>
