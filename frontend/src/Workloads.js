646d023b32b4693117ba214e133799fba12f5b47
Akram Ben Aissi <akram.benaissi@gmail.com>
2024-10-29 18:44:18 +0100

    fix

--- frontend/src/Workloads.js
+++ frontend/src/Workloads.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Typography } from '@mui/material';
 import WorkloadsList from './WorkloadsList';
-
+import useWebSocket from './useWebSocket';
 
 
 
