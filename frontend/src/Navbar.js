877d27ecd5e1639142b7e6da2dbd8e819c16493e
Akram Ben Aissi <akram.benaissi@gmail.com>
2024-10-29 12:02:52 +0100

    fix

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Button } from '@mui/material';
+import './App.css';
 
 const Navbar = () => {
   return (
