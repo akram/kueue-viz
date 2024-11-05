9375050131c5a8900f00b30f6379c6551c7f7e20
Akram Ben Aissi <akram.benaissi@gmail.com>
2024-11-05 03:19:26 +0100

    revisit navbar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -7,16 +7,13 @@ const Navbar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Link to="/" className="navbar-link">
-          <img src="/kueue-viz.png" className="navbar-logo"/>
-        </Link>
+        <Link to="/" className="navbar-link"><img src="/kueue-viz.png" className="navbar-logo"/></Link>
         <Button color="inherit" component={Link} to="/">Dashboard</Button>
         <Button color="inherit" component={Link} to="/workloads">Workloads</Button>
         <Button color="inherit" component={Link} to="/local-queues">Local Queues</Button>
         <Button color="inherit" component={Link} to="/cluster-queues">Cluster Queues</Button>
-        <Button color="inherit" component={Link} to="/resource-flavors">Resource Flavors</Button>
         <Button color="inherit" component={Link} to="/cohorts">Cohorts</Button>
-
+        <Button color="inherit" component={Link} to="/resource-flavors">Resource Flavors</Button>
       </Toolbar>
     </AppBar>
   );
